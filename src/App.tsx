50be0532859f4e3d1fb3bdeb2f4b4805ab1a656f
burrito-mode <jpantsniner@gmail.com>
2021-11-05 14:50:20 -0600

    initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
+import * as React from "react";
 import Logo from './Logo';
+import { Link, Routes, Route, Outlet } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <Logo />
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link> {" "}
+        <Link to="/about">About</Link> {" "}
+        <Link to="/login">Login</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+function Home(){
+  return (
+      <div>
+          <h2>Home</h2>
+          <p>Home sweet home</p>
+      </div>
+  );
+}
+
+function About(){
+  return (
+      <Logo />
+  );
+}
+
+function Login(){
+  return (
+      <Logo />
+  );
+}
